ad172ab94f1022920069bd4e9d127a9cbb5f8a12
DavinGN <220711841@students.uajy.ac.id>
2024-11-16 16:09:51 +0700

    Update Tugas_Modul5_B_Davin_11841.py

--- Tugas_Modul5_B_Davin_11841.py
+++ Tugas_Modul5_B_Davin_11841.py
@@ -5,8 +5,7 @@ from PIL import Image
 import os
 
 # Definisikan jalur model
-model_directory = r'D:\Sem 5\Pembelajaran Mesin\Praktek\Introduction to Deep Learning (Praktek)'
-model_path = os.path.join(model_directory, r'best_model.pkl')
+model_path = r'best_model.pkl'
 
 # Load the model
 if os.path.exists(model_path):
